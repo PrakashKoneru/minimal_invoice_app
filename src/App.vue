46a446e58edb9d4d22b69306b626cd5ed4d16820
Prakash Koneru <sivaprakashkoneru67@gmail.com>
2018-11-08 00:35:41 -0500

    Adding initial components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Invoice />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Invoice from './components/Invoice.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Invoice
   }
 }
 </script>
